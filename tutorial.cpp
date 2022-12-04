14a1b053935e4d2d2036044470b3efcd86600010
zzy <825830916@qq.com>
2022-12-04 20:03:34 +0800

    commit second

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,19 +1,21 @@
 // tutorial.cpp
-
+#include <TutorialConfig.h>
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
 #include <string>
-
 int main(int argc, char* argv[])
 {
     if (argc < 2) {
-        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+      // report version
+      std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+                << Tutorial_VERSION_MINOR << std::endl;
+      std::cout << "Usage: " << argv[0] << " number" << std::endl;
         return 1;
     }
 
     // convert input to double
-    const double inputValue = atof(argv[1]);
+    const double inputValue = std::stod(argv[1]);
 
     // calculate square root
     const double outputValue = sqrt(inputValue);
